f6b5fdb1fd739070c98c32a069088ce357e9b7be
Jorge alexandre de aquino mendes <jorgeaamsantos@gmail.com>
2022-04-10 19:09:19 -0300

    update, delete e correção

--- class/Usuario.php
+++ class/Usuario.php
@@ -3,9 +3,9 @@
 class Usuario {
 
     private $idusuario;
-    private $nome;
-    private $sobrenome;
-    private $momento_cadastro;
+    private $deslogin;
+    private $dessenha;
+    private $dtcadastro;
 
     public function getIdusuario(){
 
@@ -19,39 +19,39 @@ class Usuario {
 
     }
 
-    public function getNome(){
+    public function getDeslogin(){
 
-        return $this->nome;
-
-    }
-
-    public function setNome($value){
-
-        $this->nome = $value;
+        return $this->deslogin;
 
     }
 
-    public function getSobrenome(){
+    public function setDeslogin($value){
 
-        return $this->sobrenome;
-
-    }
-
-    public function setSobrenome($value){
-
-        $this->sobrenome = $value;
+        $this->deslogin = $value;
 
     }
 
-    public function getMomento_cadastro(){
+    public function getDessenha(){
 
-        return $this->momento_cadastro;
+        return $this->dessenha;
 
     }
 
-    public function setMomento_cadastro($value){
+    public function setDessenha($value){
 
-        $this->momento_cadastro = $value;
+        $this->dessenha = $value;
+
+    }
+
+    public function getDtcadastro(){
+
+        return $this->dtcadastro;
+
+    }
+
+    public function setDtcadastro($value){
+
+        $this->dtcadastro = $value;
 
     }
 
@@ -59,7 +59,7 @@ class Usuario {
 
         $sql = new Sql();
 
-        $results = $sql->select("SELECT * FROM clientes WHERE idusuario = :ID", array(
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
 
             ":ID"=>$id
 
@@ -67,14 +67,121 @@ class Usuario {
 
         if (count($results) > 0) {
 
-            $row = $results[0];
-
-            $this->setIdusuario($row['idusuario']);
-            $this->setNome($row['nome']);
-            $this->setSobrenome($row['sobrenome']);
-            $this->setMomento_cadastro(new DateTime($row['momento_cadastro']));
+            $this->setData($results[0]);
 
         }
+
+    }
+
+    public static function getList(){
+
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
+    }
+
+    public static function search($login){
+
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+
+            ':SEARCH'=>"%".$login."%"
+
+        ));
+
+    }
+
+    public function login($login, $password){
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+
+            ":LOGIN"=>$login,
+            ":PASSWORD"=>$password,
+
+        ));
+
+        if (count($results) > 0) {
+
+            $this->setData($results[0]);
+
+        } else {
+
+            throw new Exception("Login e/ou senha inválidos.");
+
+        }
+
+    }
+
+    public function setData($data){
+
+        $this->setIdusuario($data['idusuario']);
+        $this->setDeslogin($data['deslogin']);
+        $this->setDessenha($data['dessenha']);
+        $this->setDtcadastro(new DateTime($data['dtcadastro']));
+
+    }
+
+    public function insert(){
+
+        $sql = new Sql();
+
+        $results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+
+            ':LOGIN'=>$this->getDeslogin(),
+            ':PASSWORD'=>$this->getDessenha()
+
+        ));
+
+        if (count($results) > 0) {
+
+            $this->setData($results[0]);
+
+        }
+
+    }
+
+    public function update($login, $password){
+
+        $this->setDeslogin($login);
+        $this->setDessenha($password);
+
+        $sql = new Sql();
+
+        $sql->query("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(
+
+            ':LOGIN'=>$this->getDeslogin(),
+            ':PASSWORD'=>$this->getDessenha(),
+            ':ID'=>$this->getIdusuario()
+
+        ));
+
+    }
+
+    public function delete(){
+
+        $sql = new Sql();
+
+        $sql->query("DELETE * FROM tb_usuarios WHERE idusuario = :ID", array(
+
+            ':ID'=>$this->getIdusuario()
+
+        ));
+
+        $this->setIdusuario(0);
+        $this->setDeslogin("");
+        $this->setDessenha("");
+        $this->setDtcadastro(new DateTime());
+
+    }
+
+    public function __construct($login = "", $password = ""){
+
+        $this->setDeslogin($login);
+        $this->setDessenha($password);
 
     }
 
@@ -83,9 +190,9 @@ class Usuario {
         return json_encode(array(
 
             "idusuario"=>$this->getIdusuario(),
-            "nome"=>$this->getNome(),
-            "sobrenome"=>$this->getSobrenome(),
-            "momento_cadastro"=>$this->getMomento_cadastro()->format("d/m/Y H:i:s")
+            "deslogin"=>$this->getDeslogin(),
+            "dessenha"=>$this->getDessenha(),
+            "dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
 
         ));
 
--- index.php
+++ index.php
@@ -2,14 +2,49 @@
 
 require_once("config.php");
 
-/*$sql = new Sql();
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadbyId(3);
+//echo $root;
 
-$usuarios = $sql->select("SELECT * FROM clientes");
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo json_encode($usuarios);*/
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
 
-$root = new Usuario();
-$root->loadbyId(3);
-Echo $root;
+//Carrega um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("root", "!@#$");
+
+//echo $usuario;
+
+/*
+Criando um novo usuário
+$aluno = new Usuario("aluno", "@lun0");
+$aluno->insert();
+echo $aluno;
+*/
+
+/*
+Alterar um usuário
+$usuario = new Usuario();
+
+$usuario->loadById(8);
+
+$usuario->update("professor", "!@#$%¨&*");
+
+echo $usuario;
+*/
+
+$usuario = new Usuario();
+
+$usuario->loadById(7);
+
+$usuario->delete();
+
+echo $usuario;
 
 ?>
